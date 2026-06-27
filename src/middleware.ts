946318c3570eaffcbe7b185afdeb54cc34b51a3c
vika oneil <oneilcode111@gmail.com>
2026-06-27 23:54:17 +1000

    feat: add i18n localization

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+// src/middleware.ts
+import createMiddleware from 'next-intl/middleware';
+
+export default createMiddleware({
+  locales: ['en', 'ru'],
+  defaultLocale: 'en',
+  localePrefix: 'always',
+});
+
+export const config = {
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
+};
