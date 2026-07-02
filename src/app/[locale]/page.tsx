64fe895e05bcfd277fdb86927edebc0d627ad37c
vika oneil <oneilcode111@gmail.com>
2026-07-02 17:25:26 +1000

    feat: add basic Editor component with CodeMirror

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,16 @@
+"use client"
+
+import Editor from '@/components/features/Editor';
+import { useState } from 'react';
+
 export default function Home() {
-  return <div>Swagger Editor App</div>;
+  const [schema, setSchema] = useState(''); // текст схемы
+const [format, setFormat] = useState<'json' | 'yaml'>('yaml');
+
+  return <Editor 
+  value={schema}
+  onChange={setSchema}
+  format={format}
+  errors={[]}
+/>
 }
