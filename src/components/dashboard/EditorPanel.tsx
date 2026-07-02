75a2856643fb33e01b50bb3c087a4ced7a119314
vika oneil <oneilcode111@gmail.com>
2026-07-02 23:52:00 +1000

    feat: add handleChangeEditor function

--- src/components/dashboard/EditorPanel.tsx
+++ src/components/dashboard/EditorPanel.tsx
@@ -1,14 +1,31 @@
 import { Box, ScrollArea } from '@mantine/core';
+import Editor from '@/components/features/Editor';
+import { useState } from 'react';
+import detectFormat, { DataFormat } from '@/utils/detectFormat';
+import { DATA_FORMATS } from '@/constants';
 
-interface EditorPanelProps {
-  children?: React.ReactNode;
-}
+export function EditorPanel() {
+  const [schema, setSchema] = useState(''); 
+  const [format, setFormat] = useState<DataFormat>(DATA_FORMATS.YAML);
 
-export function EditorPanel({ children }: EditorPanelProps) {
+  function handleChangeEditor (newText: string) {
+    setSchema(newText);
+    const newFormatResult = detectFormat(newText);
+
+    if (newFormatResult) {
+      setFormat(newFormatResult);
+    }
+  }
+
   return (
     <ScrollArea h="100%" type="auto" bg="#1e90ff33">
-      <Box p={12} style={{ minHeight: 0 }}>
-        {children || 'Editor Panel Content'}
+      <Box p={5} style={{ minHeight: 0 }}>
+        <Editor 
+          value={schema}
+          onChange={handleChangeEditor}
+          format={format}
+          errors={[]}
+        />
       </Box>
     </ScrollArea>
   );
--- src/utils/detectFormat.ts
+++ src/utils/detectFormat.ts
@@ -1,21 +1,19 @@
-import yaml from 'js-yaml';
+import { DATA_FORMATS } from '@/constants';
+import * as yaml from 'js-yaml';
 
-export default function detectFormat (text: string): 'json' | 'yaml' | null {
-  if (text.length === 0 ) return null;
+export type DataFormat = typeof DATA_FORMATS[keyof typeof DATA_FORMATS]; 
+
+export default function detectFormat (text: string): DataFormat | null {
 
   try {
     JSON.parse(text); 
-    return 'json';
-  } catch {
-  
-  }
+    return DATA_FORMATS.JSON;
+  } catch {}
 
   try {
     yaml.load(text); 
-    return 'yaml';
-  } catch {
-    
-  }
+    return DATA_FORMATS.YAML;
+  } catch {}
 
   return null;
 }
