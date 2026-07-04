717c047e4095393eb5286209d8e8137e4e9b3820
vika oneil <oneilcode111@gmail.com>
2026-07-04 22:11:05 +1000

    feat: add localization for SaveSchemaButton

--- src/components/features/SaveSchemaButton/SaveSchemaButton.tsx
+++ src/components/features/SaveSchemaButton/SaveSchemaButton.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@mantine/core';
+import { useTranslations } from 'next-intl';
 
 export default function SaveSchemaButton() {
+  const t = useTranslations('SaveSchemaButton');
   return(
     <Button>
-      Save
+      {t('title')}
     </Button>  
   );
 }
