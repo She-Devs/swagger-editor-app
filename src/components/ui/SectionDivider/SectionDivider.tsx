d1ce059d33f413e63f33a60ec4cee19cea9a763f
Marta <emeraldraven27@gmail.com>
2026-07-07 00:49:00 +0300

    feat: add divider component

---
+++ src/components/ui/SectionDivider/SectionDivider.tsx
@@ -0,0 +1,24 @@
+import { Box, Divider } from '@mantine/core';
+import { IconSparkles } from '@tabler/icons-react';
+
+export function SectionDivider() {
+  return (
+    <Box
+      display="flex"
+      style={{
+        alignItems: 'center',
+        gap: '20px',
+      }}
+    >
+      <Divider style={{ flex: 1 }} />
+
+      <IconSparkles
+        size={22}
+        stroke={1.5}
+        color="var(--mantine-color-violet-5)"
+      />
+
+      <Divider style={{ flex: 1 }} />
+    </Box>
+  );
+}
